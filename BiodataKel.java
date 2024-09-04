58653648c67ed88e561ee9e30ebbdce1cc0adf8a
Minzizz <azizsudjud@gmail.com>
2024-09-04 16:27:18 +0700

    bikin biodata (ilham a ryan b)

---
+++ BiodataKel.java
@@ -0,0 +1,11 @@
+public class BiodataKel {
+
+    public static void main(String[] args) {
+        System.out.println("Biodata Mahasiswa");
+        System.out.println("\nNama       :");
+        System.out.println("NIM          :");
+        System.out.println("Tanggal Lahir:");
+        System.out.println("Alamat       :");
+
+    }
+}
